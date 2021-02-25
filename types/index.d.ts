9490a246f4976309e23fcc4a28822ab5c0f6c2b0
Nathan Barrett <nathan.barrett@gmail.com>
2021-02-25 03:53:07 -0600

    Stronger Typescript Typings (#43)
    * stronger typings for options and events
    
    * add state to param

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,19 +9,126 @@ type Stats = {
   speed: number;
 }
 
-export const DH_STATES: {
-  IDLE: 'IDLE';
-  SKIPPED: 'SKIPPED';
-  STARTED: 'STARTED';
-  DOWNLOADING: 'DOWNLOADING';
-  RETRY: 'RETRY';
-  PAUSED: 'PAUSED';
-  RESUMED: 'RESUMED';
-  STOPPED: 'STOPPED';
-  FINISHED: 'FINISHED';
-  FAILED: 'FAILED';
+export enum DH_STATES {
+  IDLE = 'IDLE',
+  SKIPPED = 'SKIPPED',
+  STARTED = 'STARTED',
+  DOWNLOADING = 'DOWNLOADING',
+  RETRY = 'RETRY',
+  PAUSED = 'PAUSED',
+  RESUMED = 'RESUMED',
+  STOPPED = 'STOPPED',
+  FINISHED = 'FINISHED',
+  FAILED = 'FAILED',
 }
 
+interface BaseStats {
+  /** total file size got from the server */
+  totalSize: number;
+  /** original file name */
+  fileName: string;
+  /** original path name */
+  filePath: string;
+  /** the downloaded amount */
+  downloadedSize: number;
+}
+
+interface DownloadInfoStats extends BaseStats {
+  /** if the download is a resume */
+  isResumed: boolean; 
+}
+
+interface DownloadEndedStats extends BaseStats {
+  /** total size of file on the disk */
+  onDiskSize: number;
+  /** true/false if the download endend but still incomplete */
+  incomplete: boolean;
+}
+
+interface FileRenamedStats {
+  /** modified path name */
+  path: string;
+  /** modified file name */
+  fileName: string;
+  /** original path name */
+  prevPath: string;
+  /** original file name */
+  prevFileName: string;
+}
+
+interface ErrorStats {
+  /** Error message */
+  message: string;
+  /** Http status response if available */
+  status?: string;
+  /** Http body response if available */
+  body?: string;
+}
+interface DownloadEvents {
+  /** Emitted when the .start method is called */
+  start: () => any;
+  /** Emitted when the download is skipped because the file already exists */
+  skip: (stats: BaseStats) => any;
+  /** Emitted when the download starts */
+  download: (stats: DownloadInfoStats) => any;
+  /**	Emitted every time gets data from the server */
+  progress: (stats: Stats) => any;
+  /** The same as progress but emits every 1 second while is downloading */
+  "progress.throttled": (stats: Stats) => any;
+  /** Emitted when the download fails and retry is enabled */
+  retry: (attempt: any, retryOptions: RetryOptions) => any;
+  /** Emitted when the downloading has finished */
+  end: (stats: DownloadEndedStats) => any;
+  /** Emitted when there is any error */
+  error: (stats: ErrorStats) => any;
+  /**	Emitted when the underlying socket times out from inactivity. */
+  timeout: () => any;
+  /** Emitted when the .pause method is called */
+  pause: () => any;
+  /** Emitted when the .resume method is called */
+  resume: (isResume: boolean) => any;
+  /** Emitted when the .stop method is called */
+  stop: () => any;
+  /** Emitted when '(number)' is appended to the end of file, this requires override:false opt, callback(filePaths) */
+  renamed: (stats: FileRenamedStats) => any;
+  /** Emitted when the state changes */
+  stateChanged: (state: DH_STATES) => any;
+}
+type FilenameCallback = (fileName: string, filePath: string) => string;
+interface FilenameDefinition {
+  name: string;
+  ext: string;
+}
+interface RetryOptions {
+  maxRetries: number;
+  /** in milliseconds */
+  delay: number;
+}
+interface OverrideOptions {
+  skip?: boolean;
+  skipSmaller?: boolean;
+}
+interface DownloaderHelperOptions {
+  /** Request Method Verb */
+  method?: "GET" | "PUT" | "POST" | "DELETE" | "OPTIONS",
+  /** Custom HTTP Header ex: Authorization, User-Agent */
+  headers?: object;
+  /** Custom filename when saved */
+  fileName?: string | FilenameCallback | FilenameDefinition;
+  retry?: boolean | RetryOptions;
+  /** If the server does not return the "accept-ranges" header, can be force if it does support it */
+  forceResume?: boolean;
+  /** remove the file when is stopped (default:true) */
+  removeOnStop?: boolean;
+  /** remove the file when fail (default:true) */
+  removeOnFail?: boolean;
+  /** Behavior when local file already exists */
+  override?: boolean | OverrideOptions;
+  /** Override the http request options */
+  httpRequestOptions?: object;
+  /** Override the https request options, ex: to add SSL Certs */
+  httpsRequestOptions?: object;
+}
 export class DownloaderHelper extends EventEmitter {
   /**
    * Creates an instance of DownloaderHelper.
@@ -30,7 +137,7 @@ export class DownloaderHelper extends EventEmitter {
    * @param {Object} [options={}]
    * @memberof DownloaderHelper
    */
-  constructor(url: string, destFolder: string, options?: object);
+  constructor(url: string, destFolder: string, options?: DownloaderHelperOptions);
 
   /**
    *
@@ -122,5 +229,12 @@ export class DownloaderHelper extends EventEmitter {
    * @returns {Promise<{name:string, total:number}>}
    * @memberof DownloaderHelper
    */
-  getTotalSize(): Promise<{name: string; total: number}>;
+  getTotalSize(): Promise<{ name: string; total: number }>;
+  
+  /**
+   * Subscribes to events
+   * 
+   * @memberof EventEmitter
+   */
+  on<E extends keyof DownloadEvents>(event: E, callback: DownloadEvents[E]): any;
 }
